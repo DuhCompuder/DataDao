a97228490ac76c4a933208c8f9b8f9f1d05d6b70
duhcompuder <gattlingjack@gmail.com>
2023-01-26 01:51:12 -0500

    updated network configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,34 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  networks: {
+    wallaby: {
+      url: "https://wallaby.node.glif.io/rpc/v0",
+      chainId: 31415,
+      accounts: [],
+    },
+    hyperspace: {
+      url: "https://api.hyperspace.node.glif.io/rpc/v0",
+      chainId: 3141,
+      accounts: [],
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  typechain: {
+    outDir: "src/types",
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ["externalArtifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false, // defaults to false
+  },
 };
 
 export default config;
